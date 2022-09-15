6de296c8ff2177a0f2d6481817e01cca9f9f3677
shin <s.suzuki.sys22@morijyobi.ac.jp>
2022-09-15 15:26:50 +0900

    pra2.javaを新規作成しました。
    Hello Worldを100回出力するプログラムを作成しました。

--- pra2.java
+++ pra2.java
@@ -1,5 +1,7 @@
-public static void main(String[] args) {
-	for(int i=0; i<100; i++) {
-		System.out.print("Hello World");
+public class pra2 {
+	public static void main(String[] args) {
+		for(int i=0; i<100; i++) {
+			System.out.print("Hello World");
+		}
 	}
 }
